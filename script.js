76e5be606e15d560bdfce473453ddec103f70776
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-04-25 17:44:48 -0400

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+function a() { 
+	return new Promise ((resolve, reject)=>{
+
+		
+})
+	
+}
+fetch("https://picsum.photos/id/237/200/300").then ((res)=>res.json())
+ .then((data)=>{
+    
+ }
